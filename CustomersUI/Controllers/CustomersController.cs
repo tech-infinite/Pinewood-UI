a5df4cccea6510407e8e6f91ef75cd76eda57b6c
Supraja Ramaraju <test.email@example.com>
2024-09-10 19:25:05 +0100

    Added solution to the repository

---
+++ CustomersUI/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using CustomersUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CustomersUI.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly HttpClient httpClient;
+        private readonly string _urlAPI = "";
+
+        public CustomersController(ILogger<CustomersController> logger)
+        {
+            
+
+        public async Task<IActionResult> Edit()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Delete()
+        {
+            return View();
+        }
+
+        
+    }
+}
